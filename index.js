c6d81a1ffc8035de060d82183ce7d65e01418d43
[elijahchen] <[code.elijahchen@gmail.com]>
2017-08-24 18:07:45 -0400

    -Fixed a minor scoping issue

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
 const { app, BrowserWindow, ipcMain } = electron;
 
+let mainWindow;
+
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
